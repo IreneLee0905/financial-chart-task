a1521abcb84432aa388e0e1310c280e91fd02d1e
IreneLee0905 <f228300k@gmail.com>
2019-06-27 12:41:53 +1000

    add portfolio restful api

--- charts/views.py
+++ charts/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework import viewsets
 from charts.models import Stock
 from charts.serializers import StockSerializer
+from rest_framework import viewsets
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -13,3 +13,22 @@ class StockViewSet(viewsets.ModelViewSet):
     serializer_class = StockSerializer
     # serializer.data
 
+
+@csrf_exempt
+def portfolio(request):
+    queryset = Stock.objects.all().order_by('date')
+    profit = 0
+    min_num = 0
+    max_num = 0
+    for i in range(len(queryset) - 1, -1, -1):
+        for j in range(i - 1, -1, -1):
+            if queryset[i].data - queryset[j].data > profit:
+                profit = queryset[i].data - queryset[j].data
+                max_num = i
+                min_num = j
+    return JsonResponse({
+        'buy_date': queryset[min_num].date,
+        'sell_date': queryset[max_num].date,
+    })
+
+#
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,14 +16,19 @@ Including another URLconf
 from django.urls import include, path
 from rest_framework import routers
 from charts import views
+from django.contrib import admin
+
 
 router = routers.DefaultRouter()
 router.register(r'stock', views.StockViewSet)
-# router.register(r'groups', views.GroupViewSet)
+
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('portfolio/', views.portfolio),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
